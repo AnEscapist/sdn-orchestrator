fdc0e735cbfd80c31f96044325f5d2da4a6641f5
tl970m <tl970m@att.com>
2019-06-27 10:05:23 -0400

    adding flask and socketio

--- ucpe/zmq/frontend_zmq_pubsub.py
+++ ucpe/zmq/frontend_zmq_pubsub.py
@@ -4,6 +4,9 @@ import sys
 import time
 import threading
 import json
+from flask import Flask, render_template 
+from flask_socketio import SocketIO, emit 
+
 
 import signal
 
@@ -22,10 +25,29 @@ def sub_response():
         if socket_sub.get(zmq.RCVMORE) and topic == topicfilter:
             messagedata = socket_sub.recv()
             print(messagedata)
+            socketio.emit('vm_state', {'vm_state': messagedata})
+			
+@socketio.on('get_vm_state') 
+def get_data(messagedata): 
+    messagedata = json.dumps(messagedata)
+    topic = "test-id"
+    #messagedata = {"method": "libvirt_controller_get_vm_state", "params": {"body":{"username": "potato", "hostname": "10.10.81.100", "vm_name": "test", "autostart": 1, "save_path": "/home/potato/save_path.test"}}, "jsonrpc": "2.0", "id": 0}
+    #messagedata = json.dumps(messagedata)
+    print("%s %s" % (topic, messagedata))
+    socket_pub.send_string(topic, zmq.SNDMORE)
+    socket_pub.send_string(messagedata)
+
 
 
 port_pub = "5559"
 port_sub = "5570"
+
+port_socketio = "5000"
+host = socket.gethostbyname(socket.gethostname())
+
+app = Flask(__name__) 
+socketio = SocketIO(app) 
+
 
 # Socket to publish requests
 context_pub = zmq.Context()
@@ -39,13 +61,12 @@ t1 = threading.Thread(target=sub_response)
 # starting thread 1 
 t1.start() 
 
+socketio.run(app, host=host, port=port_socketio) 
+
+"""
+
 time.sleep(1)
-topic = "test-id"
-messagedata = {"method": "libvirt_controller_get_vm_state", "params": {"body":{"username": "potato", "hostname": "10.10.81.100", "vm_name": "test", "autostart": 1, "save_path": "/home/potato/save_path.test"}}, "jsonrpc": "2.0", "id": 0}
-messagedata = json.dumps(messagedata)
-print("%s %s" % (topic, messagedata))
-socket_pub.send_string(topic, zmq.SNDMORE)
-socket_pub.send_string(messagedata)
+
 print("%s %s" % (topic, messagedata))
 socket_pub.send_string(topic, zmq.SNDMORE)
 socket_pub.send_string(messagedata)
@@ -62,7 +83,7 @@ print("%s %s" % (topic, messagedata))
 socket_pub.send_string(topic, zmq.SNDMORE)
 socket_pub.send_string(messagedata)
 
-
+"""
 
 
 
