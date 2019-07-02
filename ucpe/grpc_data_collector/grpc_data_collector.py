189f8c5a03dc3cc82cca24ab8f800f238e72a06c
jesse lin <jesse.lin67@gmail.com>
2019-07-02 16:00:52 -0400

    bugfix

--- ucpe/grpc_data_collector/grpc_data_collector.py
+++ ucpe/grpc_data_collector/grpc_data_collector.py
@@ -22,38 +22,43 @@ class gRPCDataController(object):
 
     @staticmethod
     def grpc_get_hugepages_totalmem(self, hostname=HOSTNAME, port=PORT):
-        get_execute(self, "hugepages total", hostname, port)
+        return get_execute(self, "hugepages total", hostname, port)
 
     @staticmethod
     def grpc_get_hugepages_freemem(self, hostname=HOSTNAME, port=PORT):
-        get_execute(self, "hugepages free", hostname, port)
+        return get_execute(self, "hugepages free", hostname, port)
 
     @staticmethod
     def grpc_get_totalmem(self, hostname=HOSTNAME, port=PORT):
-        get_execute(self, "memory total", hostname, port)
+        return get_execute(self, "memory total", hostname, port)
 
     @staticmethod
     def grpc_get_availmem(self, hostname=HOSTNAME, port=PORT):
-        get_execute(self, "memory available", hostname, port)
+        return get_execute(self, "memory available", hostname, port)
 
 
 # ==================== private functions ===============================
 def interpret_params(controller, input_string):
     tmp = input_string.split(" ")
     count = len(tmp)
+    # print(tmp[0])
+    # print(tmp[1])
     controller.params['body']['command'] = tmp[0]
     controller.params['body']['str_request'] = tmp[1]
     if count > 2:
         controller.params['body']['str_params1'] = tmp[2]
         if count > 3:
             controller.params['body']['str_params2'] = tmp[3]
+    # print(str(controller.params))
+    return controller
 
 
 def get_execute(controller, input_string, hostname, port):
-    interpret_params(controller, input_string)
+    controller = interpret_params(controller, input_string)
     controller.params['body']['hostname'] = hostname
     controller.params['body']['port'] = port
-    return data_client.run('get', controller.params['body'])
+    body = controller.params['body']
+    return data_client.run('get', **body)
 
 
 def modify_execute(controller, input_string, hostname, port):
