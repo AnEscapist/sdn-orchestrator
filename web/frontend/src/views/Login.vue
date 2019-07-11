ba382fdf49e1f832fe5669617efb79afde507ad9
AnEscapist <ustcjzq@gmail.com>
2019-07-11 16:08:48 -0400

    modify login

--- web/frontend/src/views/Login.vue
+++ web/frontend/src/views/Login.vue
@@ -2,27 +2,33 @@
 <div class="login">
     <div class="item">
         <img alt="att-logo" src="../assets/att.png" height="220" width="500"><br>
+        <br>
 
-        <div class="user">
-            <div class="col-md-6 col-md-offset-5">
-                <div class="input-group">
-                    <span class="input-group-text" id="basic-addon1"><font-awesome-icon :icon="['fas', 'user']" size=lg /></span>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        <center>
+            <div class="user">
+                <div class="col-md-5 col-md-offset-5">
+                    <div class="input-group">
+                        <span class="input-group-text" id="basic-addon1">
+                            <font-awesome-icon :icon="['fas', 'user']" size=lg /></span>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="password">
-            <div class="col-md-6 col-md-offset-5">
-                <div class="input-group">
-                    <span class="input-group-text" id="basic-addon1"><font-awesome-icon :icon="['fas', 'lock']" size=lg /></span>
-                    <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+            <div class="password">
+                <div class="col-md-5 col-md-offset-5">
+                    <div class="input-group">
+                        <span class="input-group-text" id="basic-addon1">
+                            <font-awesome-icon :icon="['fas', 'lock']" size=lg /></span>
+                        <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                    </div>
                 </div>
             </div>
-        </div>
+        </center>
 
 
         <br />
-        <button type="button" class='btn btn-primary btn-lg' @click='toDocker'><font-awesome-icon :icon="['fas', 'sign-in-alt']" size=sm /> Login</button>
+        <button type="button" class='btn btn-primary btn-lg' @click='toDocker'>
+            <font-awesome-icon :icon="['fas', 'sign-in-alt']" size=sm /> Login</button>
         <br>
         <button type="button" class="btn btn-link btn-sm">Forget password</button> |
         <button type="button" class="btn btn-link btn-sm">Create account</button>
@@ -41,7 +47,7 @@ export default {
         }
     },
     methods: {
-        toDocker: function(){
+        toDocker: function() {
             this.$router.push({
                 path: "/docker"
             })
@@ -59,15 +65,6 @@ export default {
 
 .item {
     padding-top: 100px;
-}
-
-.user {
-    margin-left: 650px;
-    margin-top: 50px;
-}
-.password {
-    margin-left: 650px;
-    margin-top: 2px;
 }
 
 
