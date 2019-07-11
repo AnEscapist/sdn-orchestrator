95c60c6dfce43c80fe7016f00d7e053cabb6b175
Roger Jin <rogerjin@mit.edu>
2019-07-11 16:37:56 -0400

    move server off agent

--- web/backend/server.py
+++ web/backend/server.py
@@ -14,33 +14,19 @@ port_pub = "5559"
 port_sub = "5570"
 TIMEOUT = 1000 #todo: decide
 CONTROLLER_ID = "test-id"
+BROKER_IP = "10.10.81.200" #todo: make this not global
 #todo: set topic to something reasonable, like request id or timestampidk
 
 
 def call_ucpe_function(messagedata, controller_id="test-id", ucpe_sn="test-sn"):
     context_pub = zmq.Context()
     socket_pub = context_pub.socket(zmq.PUB)
-    socket_pub.connect("tcp://localhost:%s" % port_pub)
+    socket_pub.connect("tcp://%s:%s" % (BROKER_IP, port_pub))
     q = queue.Queue()
     response_thread = threading.Thread(target=sub_response, args=(q, ucpe_sn))
     response_thread.start() #todo: figure out how to configure timeout
     time.sleep(1) #todo: BAD
     send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
-    # send_request(messagedata, controller_id, socket_pub)
     response = q.get(timeout=TIMEOUT)
     print(response)
     return response
@@ -60,7 +46,7 @@ def sub_response(queue, ucpe_sn):
     context_sub = zmq.Context()
     socket_sub = context_sub.socket(zmq.SUB)
     print("Collecting updates from server...")
-    socket_sub.connect("tcp://localhost:%s" % port_sub)
+    socket_sub.connect("tcp://%s:%s" % (BROKER_IP, port_sub))
     socket_sub.setsockopt_string(zmq.SUBSCRIBE, ucpe_sn)
     while True: #does this block?
         print("in while loop")
