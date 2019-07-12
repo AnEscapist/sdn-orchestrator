b7917366952c8dd34e37dbecaf6e07ce788da05f
Roger Jin <rogerjin@mit.edu>
2019-07-12 15:54:27 -0400

    fix store

--- web/frontend/src/store.ts
+++ web/frontend/src/store.ts
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import docker from './store/modules/';
-import data from './store/modules/';
-import vms from './store/modules/';
+// import docker from './store/modules/';
+// import data from './store/modules/';
+// import vms from './store/modules/';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    docker,
-    data,
-    vms
+    // docker,
+    // data,
+    // vms
   }
 });
 
