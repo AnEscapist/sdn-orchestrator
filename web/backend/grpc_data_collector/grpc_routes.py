fb82caf569014564efc25c29ec0fd34f9f7f605c
jesse lin <jesse.lin67@gmail.com>
2019-07-12 09:51:56 -0400

    grpc routes

--- web/backend/grpc_data_collector/grpc_routes.py
+++ web/backend/grpc_data_collector/grpc_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, abort, jsonify
 from jinja2 import TemplateNotFound
 
-grpc_routes = Blueprint('page', __name__, template_folder='templates')
+grpc_routes = Blueprint('grpc_page', __name__, template_folder='templates')
 
 @grpc_routes.route('/data-collect/dashboard')
 def hello():
