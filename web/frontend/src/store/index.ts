9fce019ee0ca9123452d1abb1d26aef311bfdf21
Roger Jin <rogerjin@mit.edu>
2019-07-12 16:43:26 -0400

    fixed store

---
+++ web/frontend/src/store/index.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import data from './modules/data';
+import docker from './modules/docker';
+import vms from './modules/vms';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {
+    product,
+    cart
+  }
+});
