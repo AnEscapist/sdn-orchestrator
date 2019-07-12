e3136ecb8f0ce8ea02484f0bd15320b4f42e31c7
Roger Jin <rogerjin@mit.edu>
2019-07-12 11:53:33 -0400

    some changes

---
+++ web/frontend/src/components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="nav">
+        <!-- <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> | -->
+
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/docker">Docker</router-link>
+        <router-link to="/libvirt">Virtual Machines</router-link>
+        <router-link to="/grpc">GRPC</router-link>
+        <DSidebar></DSidebar>
+
+    </div>
+</template>
+
+<script>
+    import
+    export default {
+        name: "navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
