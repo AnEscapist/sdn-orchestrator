d9b6bd938450a2598f53506ca803301dadb57b3d
Roger Jin <rogerjin@mit.edu>
2019-07-15 11:48:48 -0400

    new server without deadlock problem

--- web/backend/server.py
+++ web/backend/server.py
@@ -6,11 +6,16 @@ from flask import Flask, escape, request, jsonify
 from web.backend.libvirt.libvirt_routes import libvirt_routes
 from web.backend.docker.docker_routes import docker_routes
 from web.backend.grpc_data_collector.grpc_routes import grpc_routes
+import web.backend.zmq_web as zmq_web
 
 app = Flask(__name__)
 app.register_blueprint(libvirt_routes)
 app.register_blueprint(docker_routes)
 app.register_blueprint(grpc_routes)
 
-if __name__ == "__main__":
+def serve():
+    zmq_web.start()
     app.run(threaded=True)
+
+if __name__ == "__main__":
+    serve()
