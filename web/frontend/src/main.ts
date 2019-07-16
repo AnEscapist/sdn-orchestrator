595622237702652b28507e61f28134a6ebbedf55
Roger Jin <rogerjin@mit.edu>
2019-07-16 09:55:57 -0400

    Revert "move router.ts to router folder"
    This reverts commit 97bcd7e7e33ba823368c70dde66212ba99549323.

--- web/frontend/src/main.ts
+++ web/frontend/src/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router/router';
+import router from './router';
 import store from './app/store';
 import './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
