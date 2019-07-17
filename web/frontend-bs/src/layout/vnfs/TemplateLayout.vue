ab8153644e972ba089ccb4877b98f3ed761e9010
Roger Jin <rogerjin@mit.edu>
2019-07-17 19:20:16 -0400

    add ucpe routes

---
+++ web/frontend-bs/src/layout/vnfs/TemplateLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout
+    :title="title"
+    :home="home"
+    :logo="logo"
+    :sidebar-background-image="sidebar.backgroundImage"
+    :sidebar-color="sidebar.color"
+    :sidebar-items="sidebar.items"></Layout>
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+  import Layout from '../Layout.vue'
+  import templateSidebar from './templateSidebar'
+
+  export default {
+    components: {
+      Layout
+    },
+    data() {
+      return {
+        title: 'template',
+        home: '/admin/overview',
+        logo: 'img/sample-pages-logo.png', //public/img/sample-pages-logo.png
+        sidebar: templateSidebar
+      }
+    }
+  }
+
+</script>
