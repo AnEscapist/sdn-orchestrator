b0859e8c7191c0d319e6d6524b08bbdfa536e732
Roger Jin <rogerjin@mit.edu>
2019-07-18 18:22:56 -0400

    hack for multiple controllers with same id

--- web/backend/zmq_web.py
+++ web/backend/zmq_web.py
@@ -142,8 +142,9 @@ def handleResponses():
         response = json.loads(message)
         request_id = request_id_from_topic(topic)
         with response_queues_lock:
-            response_queue = response_queues[request_id]
-        response_queue.put(response)
+            if request_id in response_queues:
+                response_queue = response_queues[request_id]
+                response_queue.put(response)
 
 
 def handleFiniteResponses(iterations):
