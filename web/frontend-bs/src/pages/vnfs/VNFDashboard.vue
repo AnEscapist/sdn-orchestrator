af2d4cfbcb83611f290bddfea43d8e6d40e47133
Roger Jin <rogerjin@mit.edu>
2019-07-18 09:39:44 -0400

    vnf summary

--- web/frontend-bs/src/pages/vnfs/VNFDashboard.vue
+++ web/frontend-bs/src/pages/vnfs/VNFDashboard.vue
@@ -1,112 +1,24 @@
 <template>
   <div class="content">
     <div class="container-fluid">
-      <card>
-        <div class="info">
-
-        </div>
-      </card>
-      <card>
-        <div class="row">
-          <div class="col-md-6">
-            <h5>Notifications Style</h5>
-            <div class="alert alert-info">
-              <span>This is a plain notification</span>
-            </div>
-            <div class="alert alert-info">
-              <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
-                <i class="nc-icon nc-simple-remove"></i>
-              </button>
-              <span>This is a notification with close button.</span>
-            </div>
-            <div class="alert alert-info alert-with-icon" data-notify="container">
-              <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
-                <i class="nc-icon nc-simple-remove"></i>
-              </button>
-              <span data-notify="icon" class="nc-icon nc-app"></span>
-              <span data-notify="message">This is a notification with close button and icon.</span>
-            </div>
-            <div class="alert alert-info alert-with-icon" data-notify="container">
-              <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
-                <i class="nc-icon nc-simple-remove"></i>
-              </button>
-              <span data-notify="icon" class="nc-icon nc-app"></span>
-              <span data-notify="message">This is a notification with close button and icon and have many lines. You can see that the icon and the close button are always vertically aligned. This is a beautiful notification. So you don't have to worry about the style.</span>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <h5>Notification states</h5>
-            <div class="alert alert-info">
-              <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
-                <i class="nc-icon nc-simple-remove"></i>
-              </button>
-              <span><b> Info - </b> This is a regular notification made with ".alert-info"</span>
-            </div>
-            <div class="alert alert-success">
-              <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
-                <i class="nc-icon nc-simple-remove"></i>
-              </button>
-              <span><b> Success - </b> This is a regular notification made with ".alert-success"</span>
-            </div>
-            <div class="alert alert-warning">
-              <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
-                <i class="nc-icon nc-simple-remove"></i>
-              </button>
-              <span><b> Warning - </b> This is a regular notification made with ".alert-warning"</span>
-            </div>
-            <div class="alert alert-danger">
-              <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
-                <i class="nc-icon nc-simple-remove"></i>
-              </button>
-              <span><b> Danger - </b> This is a regular notification made with ".alert-danger"</span>
-            </div>
-          </div>
-        </div>
-        <br>
-        <br>
-        <div class="places-buttons">
-          <div class="row justify-content-center">
-            <div class="col-6 text-center">
-              <h5>Notifications Places
-                <p class="category">Click to view notifications</p>
-              </h5>
-            </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-md-3 col-md-offset-1">
-              <button class="btn btn-default btn-block" @click="notifyVue('top', 'left')">Top Left</button>
-            </div>
-            <div class="col-md-3">
-              <button class="btn btn-default btn-block" @click="notifyVue('top', 'center')">Top Center</button>
-            </div>
-            <div class="col-md-3">
-              <button class="btn btn-default btn-block" @click="notifyVue('top', 'right')">Top Right</button>
-            </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-md-3 col-md-offset-1">
-              <button class="btn btn-default btn-block" @click="notifyVue('bottom', 'left')">Bottom Left</button>
-            </div>
-            <div class="col-md-3">
-              <button class="btn btn-default btn-block" @click="notifyVue('bottom', 'center')">Bottom Center</button>
-            </div>
-            <div class="col-md-3">
-              <button class="btn btn-default btn-block" @click="notifyVue('bottom', 'right')">Bottom Right</button>
-            </div>
-          </div>
-        </div>
-      </card>
+      <VNFSummaryPanel></VNFSummaryPanel>
+      <SampleCard></SampleCard>
+<!--      todo: notifications fo VNF status changes-->
     </div>
   </div>
 </template>
 
 <script>
     import Card from 'src/components/Cards/Card.vue'
+    import SampleCard from './SampleCard'
+    import VNFSummaryPanel from './VNFSummaryPanel'
+    import VNFInfoPanel from './VNFInfoPanel'
+
 
     export default {
       name: 'VNFDashboard',
       components: {
-        Card
+        Card, SampleCard, VNFSummaryPanel, VNFInfoPanel
       },
       data () {
         return {
@@ -133,5 +45,14 @@
 </script>
 
 <style scoped>
-
+  .info{
+    font-size: 15px;
+    padding-left: 10px;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    /*border: 1px solid rgb(180, 180, 180);*/
+    /*background: rgb(240, 240, 240);*/
+    text-align: left;;
+  }
+/*todo: consider making this global*/
 </style>
---
+++ web/frontend-bs/src/pages/vnfs/VNFSummaryPanel.vue
@@ -0,0 +1,63 @@
+<template>
+  <card>
+    <div class="info">
+<!--      <font-awesome-icon :icon="['fab', 'docker']" size=lg color='rgb(111, 111, 111)' />-->
+      <strong>VNF Summary</strong>
+      <span class="badge badge-success">Connected</span>
+<!--todo: align badge to the right-->
+      <hr>
+      <table>
+        <tr>
+          <td width='30px'></td>
+          <td width='100px'>
+            <font-awesome-icon :icon="['fas', 'info-circle']" size=lg color='rgb(111, 111, 111)' />
+            <strong> Info: </strong>
+          </td>
+          <td width='250px'>
+            <font-awesome-icon :icon="['fas', 'database']" size=sm color='rgb(111, 111, 111)' />
+            10 VNFs -
+            <font-awesome-icon :icon="['fas', 'heartbeat']" size=sm color='rgb(81, 164, 81)' />
+            6
+            <font-awesome-icon :icon="['fas', 'heartbeat']" size=sm color='rgb(237, 187, 66)' />
+            3
+            <font-awesome-icon :icon="['fas', 'heartbeat']" size=sm color='rgb(204, 68, 74)' />
+            1
+<!--            todo: add tooltips on hover-->
+          </td>
+          <td width='120px'>
+            <font-awesome-icon :icon="['fas', 'clone']" size=sm color='rgb(111, 111, 111)' />
+            23 Images
+          </td>
+          <td>
+            <font-awesome-icon :icon="['fas', 'microchip']" size=sm color='rgb(111, 111, 111)' />
+            16 CPUs
+          </td>
+        </tr>
+        <tr>
+          <td width='30px'></td>
+          <td width='100px'>
+            <font-awesome-icon :icon="['fab', 'linux']" size=lg color='rgb(111, 111, 111)' />
+            <strong> Host OS: </strong>
+          </td>
+          <td width='250px'>
+            Ubuntu 18.04
+          </td>
+<!--          todo: consider removing host cpu/os (customer knows nothing about docker so doesn't know containers run on host os or even that containers exist)-->
+        </tr>
+      </table>
+    </div>
+  </card>
+</template>
+
+<script>
+    export default {
+        name: "VNFSummaryPanel",
+        components:{
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
