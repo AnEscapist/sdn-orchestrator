e26139e591a6050e72e637ddd87d6aac8bec25f0
Roger Jin <rogerjin@mit.edu>
2019-07-18 18:51:02 -0400

    vm routes

--- web/backend/vms/vm_routes.py
+++ web/backend/vms/vm_routes.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, abort,jsonify
 from jinja2 import TemplateNotFound
 from web.backend.zmq_web import call_ucpe_function
 
-libvirt_routes = Blueprint('libvirt', __name__)
+vm_routes = Blueprint('libvirt', __name__)
 
-@libvirt_routes.route('/libvirt/hello')
+@vm_routes.route('/libvirt/hello')
 def hello():
     return jsonify(name='zhengqi')
