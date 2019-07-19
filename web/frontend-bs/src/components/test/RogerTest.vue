d0af86834f2e4a3704ce56e708021475f24a277b
Roger Jin <rogerjin@mit.edu>
2019-07-19 09:14:20 -0400

    sorry zhengqi

--- web/frontend-bs/src/components/test/RogerTest.vue
+++ web/frontend-bs/src/components/test/RogerTest.vue
@@ -25,7 +25,7 @@
     },
     data() {
       return {
-        vmTableColumns: ['Name', 'State', 'Max Memory', 'CPUs', 'CPU Time']
+        vmTableColumns: ['Name', 'State', 'Max Memory', 'Memory Usage', 'CPUs', 'CPU Time']
       }
     },
     created() {
