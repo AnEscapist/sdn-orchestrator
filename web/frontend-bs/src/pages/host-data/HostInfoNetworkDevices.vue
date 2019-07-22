93e4bcee41e732106e2b2abfe8689069f1ab151d
jesse lin <jl459k@att.com>
2019-07-22 16:58:24 -0400

    changing table again

--- web/frontend-bs/src/pages/host-data/HostInfoNetworkDevices.vue
+++ web/frontend-bs/src/pages/host-data/HostInfoNetworkDevices.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="content">
     <div class="mt-2">
-      <AgGridVue style="width: 1000px; height: 300px;"
+      <AgGridVue style="width: 1220px; height: 300px;"
                  class="ag-theme-balham"
                  :columnDefs="columnDefs"
-                 :rowData="data"
+                 :rowData="rowData"
                  :gridOptions="gridOptions"
       >
       </AgGridVue>
@@ -22,22 +22,17 @@
         },
         data () {
           return {
+            rowData: [],
+            data: [],
             columns: [...tableColumns],
             columnDefs: [
               { headerName: 'Slot', field: 'slot', sortable: true, checkboxSelection: true },
               { headerName: 'Device Name', field: 'device_name', sortable: true },
               { headerName: 'Interface', field: 'interface', sortable: true },
-              { headerName: 'Driver', field: 'driver',
-                sortable: true,
-                editable: true,
-                cellEditor: "agSelectCellEditor",
-                cellEditorParams: {
-                  values: []
-                }
-              },
+              { headerName: 'Current Driver', field: 'current_driver', sortable: false, editable: true },
+              { headerName: 'Available Drivers', field: 'drivers', sortable: false},
               { headerName: 'Driver Type', field: 'driver_type', sortable: true }
             ],
-            data: [],
             gridOptions: {
               suppressCellSelection: true
             },
@@ -51,23 +46,25 @@
         methods: {
           getDevices(){
             this.axios.get("/api/grpc/get_net_devices").then(response => {
-              var res = JSON.parse(response.data.result.return)
-              var tmp_list = []
-              // console.log(res)
+              let res = JSON.parse(response.data.result.return)
+              let tmp_list = [];
+              this.data = res
+              console.log(this.data)
               Object.values(res).forEach(function(i) {
-                var tmp_dict = {
+                let tmp_dict = {
                   'slot': i.slot,
                   'device_name': i.device_name,
                   'interface': i.interface,
-                  'driver': i.drivers,
+                  'current_driver': i.current_driver,
+                  'drivers': i.drivers,
                   'driver_type': i.driver_type
                 };
                 tmp_list.push(tmp_dict)
               });
-              this.data = tmp_list;
-              console.log(this.data)
+              this.rowData = tmp_list;
+              console.log(this.rowData)
             });
-          }
+          },
         }
     }
 </script>
--- web/frontend-bs/src/store/modules/hostData/index.js
+++ web/frontend-bs/src/store/modules/hostData/index.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
 const state = {
+  drivers: []
 };
 
 const mutations = {
+  UPDATE_DRIVERS(state, payload){
+    state.drivers = payload
+  }
 };
 
 const actions = {
+  updateDrivers({commit}, token){
+    axios.get("/api/grpc/get_net_devices").then((response) => {
+      commit('UPDATE_DRIVERS', response.data.result.return)}
+    )
+  }
 };
 
 const getters = { //note: this is for retrieving things from the state.  DO NOT put backend requests here
+  drivers: state => state.drivers
 };
 
 const methods = { //axios requests go here
