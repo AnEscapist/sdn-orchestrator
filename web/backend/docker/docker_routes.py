14bc9d80df9f43464e3f1794ce11fff1041ccd35
AnEscapist <ustcjzq@gmail.com>
2019-07-22 14:17:34 -0400

    test rename container

--- web/backend/docker/docker_routes.py
+++ web/backend/docker/docker_routes.py
@@ -48,14 +48,14 @@ def change_status():
                  "save_path": "/home/potato/save_path.test"}}, "jsonrpc": "2.0", "id": 0}
     return jsonify(call_ucpe_function(messagedata))
 
-@docker_routes.route('/docker/rename_container')
-def rename_container():
-    newName = request.args.get('newName')
-    id_name = request.args.get('id_name')
-    messagedata = {"method": "docker_controller_rename_container", "params": {
-        "body": {"newName": newName, 'id_name': id_name, "username": "potato", "hostname": "10.10.81.100", "vm_name": "test", "autostart": 1,
-                 "save_path": "/home/potato/save_path.test"}}, "jsonrpc": "2.0", "id": 0}
-    return jsonify(call_ucpe_function(messagedata))
+# @docker_routes.route('/docker/rename_container')
+# def rename_container():
+#     newName = request.args.get('newName')
+#     id_name = request.args.get('id_name')
+#     messagedata = {"method": "docker_controller_rename_container", "params": {
+#         "body": {"newName": newName, 'id_name': id_name, "username": "potato", "hostname": "10.10.81.100", "vm_name": "test", "autostart": 1,
+#                  "save_path": "/home/potato/save_path.test"}}, "jsonrpc": "2.0", "id": 0}
+#     return jsonify(call_ucpe_function(messagedata))
 
 @docker_routes.route('/docker/list_images')
 def list_images():
