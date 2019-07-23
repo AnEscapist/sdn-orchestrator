2b3c81fcabad689f95ce15a167275e1cacd75ce7
Roger Jin <rogerjin@mit.edu>
2019-07-23 17:47:29 -0400

    hugepages/cpus/memory

---
+++ ucpe/libvirt_controller/grpc/copy_image.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+
+BASE_DIRECTORY = '/var/third-party/base'
+ACTIVE_DIRECTORY = '/var/third-party/active'
+def copy_image(vm_name, image_file_name):
+    base_path = os.path.join(BASE_DIRECTORY, image_file_name)
+    image_active_dir = os.path.join(ACTIVE_DIRECTORY, vm_name)
+    os.makedirs(image_active_dir)
+    p = subprocess.Popen(['cp', base_path, image_active_dir])
+    out,err = p.communicate()
+    return out, err
+
+if __name__ == '__main__':
+    vm_name = 'test'
+    image_file_name = 'storage.qcow2'
+    copy_image(vm_name, image_file_name)
+
--- ucpe/libvirt_controller/grpc/services.py
+++ ucpe/libvirt_controller/grpc/services.py
@@ -1,12 +1,20 @@
 import ucpe.libvirt_controller.grpc.libvirt_pb2 as libvirt_pb2
 import ucpe.libvirt_controller.grpc.libvirt_pb2_grpc as libvirt_pb2_grpc
 from ucpe.libvirt_controller.grpc.blockpull import blockpull
+from ucpe.libvirt_controller.grpc.copy_image import copy_image
 
 class LibvirtServicer(libvirt_pb2_grpc.LibvirtServicer):
 
     def BlockPull(self, request, context):
         response = libvirt_pb2.Response()
         out,err = blockpull(request.domain, request.path, request.base)
+        response.out = out
+        response.error = err
+        return response
+
+    def CopyImage(self, request, context):
+        response = libvirt_pb2.Response()
+        out,err = copy_image(request.vm_name, request.image_file_name)
         response.out = out
         response.error = err
         return response
