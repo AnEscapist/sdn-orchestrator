06b34a82fd57f73c7d09509e85fde238bcaf8ad3
AnEscapist <ustcjzq@gmail.com>
2019-07-23 11:30:13 -0400

    test create container function

--- web/frontend-bs/src/pages/vnfs/docker/docker_c.vue
+++ web/frontend-bs/src/pages/vnfs/docker/docker_c.vue
@@ -131,7 +131,7 @@ import Card from '../../../components/Cards/Card.vue'
 // import LTable from '@/components/Table.vue'
 
 export default {
-    inject: ['reload'],
+  inject: ['reload'],
 
   name: 'DockerC',
 
@@ -268,6 +268,7 @@ export default {
           create_port: create_port
         }
       }).then(response => {
+        console.log(response)
         this.showCreate = false
         this.reload()
       })
