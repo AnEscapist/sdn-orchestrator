1dfbbaef6c28367db69804829f7407f906823e0b
Allen <azhang307@gatech.edu>
2019-07-23 15:39:23 -0400

    Fixed duplicate route declarations

--- web/frontend-bs/src/components/test/AllenTest.vue
+++ web/frontend-bs/src/components/test/AllenTest.vue
@@ -1,11 +1,24 @@
 <template>
-  <div id='example-3'>
-    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
-    <label for="jack">Jack</label>
-    <input type="checkbox" id="john" value="John" v-model="checkedNames">
-    <label for="john">John</label>
-    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
-    <label for="mike">Mike</label>
+  <div>
+    <table>
+      <tr>
+        <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+        <label for="jack">  Jack</label>
+        <input type="checkbox" id="john" value="John" v-model="checkedNames">
+        <label for="john">  John</label>
+        <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+        <label for="mike">  Mike</label>
+      </tr>
+      <tr>
+        <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+        <label for="jack">  Jack</label>
+        <input type="checkbox" id="john" value="John" v-model="checkedNames">
+        <label for="john">  John</label>
+        <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+        <label for="mike">  Mike</label>
+      </tr>
+    </table>
+
     <br>
     <span>Checked names: {{ checkedNames }}</span>
     <h1>{{test}}</h1>
@@ -13,7 +26,9 @@
 </template>
 <script>
   import axios from 'axios';
+  import Table from "../index";
     export default {
+        components: {Table},
         data () {
             return{
             checkedNames: [],
--- web/frontend-bs/src/routes/bcmRoutes.js
+++ web/frontend-bs/src/routes/bcmRoutes.js
@@ -2,14 +2,6 @@ import DashboardLayout from '../layout/sample-layout/TemplateLayout.vue'
 
 // Admin pages
 import BCMdash from '../pages/bcm/BCMdash.vue'
-import UserProfile from '../pages/sample-pages/UserProfile.vue'
-import TableList from '../pages/sample-pages/TableList.vue'
-import Typography from '../pages/sample-pages/Typography.vue'
-import Icons from '../pages/sample-pages/Icons.vue'
-import Maps from '../pages/sample-pages/Maps.vue'
-import Notifications from '../pages/sample-pages/Notifications.vue'
-import Upgrade from '../pages/sample-pages/Upgrade.vue'
-import Test from '../pages/sample-pages/Test.vue'
 
 const bcmRoutes = [
   {
@@ -21,46 +13,6 @@ const bcmRoutes = [
         path: 'overview',
         name: 'Overview',
         component: BCMdash
-      },
-      {
-        path: 'user',
-        name: 'User',
-        component: UserProfile
-      },
-      {
-        path: 'table-list',
-        name: 'Table List',
-        component: TableList
-      },
-      {
-        path: 'typography',
-        name: 'Typography',
-        component: Typography
-      },
-      {
-        path: 'icons',
-        name: 'Icons',
-        component: Icons
-      },
-      {
-        path: 'maps',
-        name: 'Maps',
-        component: Maps
-      },
-      {
-        path: 'notifications',
-        name: 'Notifications',
-        component: Notifications
-      },
-      {
-        path: 'test',
-        name: 'test',
-        component: Test
-      },
-      {
-        path: 'upgrade',
-        name: 'Upgrade to PRO',
-        component: Upgrade
       }
     ]
   },
