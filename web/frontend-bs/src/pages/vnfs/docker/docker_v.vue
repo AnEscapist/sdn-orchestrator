531e7d42dce773596826c504d44d9ab01a7f5ce6
AnEscapist <ustcjzq@gmail.com>
2019-07-23 20:03:25 -0400

    add list volumes function

---
+++ web/frontend-bs/src/pages/vnfs/docker/docker_v.vue
@@ -0,0 +1,104 @@
+<template>
+<div class="content">
+    <div class="container-fluid">
+      <card>
+        <font-awesome-icon :icon="['fas', 'cubes']" size=lg color='rgb(0, 0, 0)' /> <strong> Volumes</strong>
+        <hr>
+
+
+
+      </card>
+    </div>
+</div>
+</template>
+
+<script>
+import Card from '../../../components/Cards/Card.vue'
+// import { mapGetters, mapActions } from 'vuex'
+// import LTable from '@/components/Table.vue'
+
+export default {
+  inject: ['reload'],
+  name: 'DockerV',
+
+  components: {
+    Card,
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+      this.axios.get('/api/docker/list_volumes').then(response => {
+          console.log(response)
+      })
+
+
+  },
+  methods: {
+    notifyVue(verticalAlign, horizontalAlign) {
+      const color = Math.floor((Math.random() * 4) + 1)
+      this.$notifications.notify({
+        message: `<span>Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer.</span>`,
+        icon: 'nc-icon nc-app',
+        horizontalAlign: horizontalAlign,
+        verticalAlign: verticalAlign,
+        type: this.type[color]
+      })
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+.info {
+  font-size: 15px;
+  padding-left: 10px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  /*border: 1px solid rgb(180, 180, 180);*/
+  /*background: rgb(240, 240, 240);*/
+  text-align: left;
+  ;
+}
+
+a {
+  color: rgb(38, 138, 255);
+  font-family: Arial, sans-serif;
+  font-weight: bold;
+  font-size: 13px;
+}
+
+input {
+
+  border: 2px solid rgb(200, 200, 200);
+  border-radius: 4px;
+  padding-left: 8px;
+}
+
+p {
+  font-size: 12px;
+  font-weight: bold;
+}
+
+
+#containerInfoCard {
+  font-family: Arial, sans-serif;
+  font-size: 15px;
+  background-color: rgb(248, 248, 248);
+  border-bottom: 2px solid white;
+  height: 25px;
+  padding-top: 3px;
+  padding-bottom: 3px;
+
+}
+
+#containerInfoCard:hover {
+  background-color: rgb(233, 233, 233);
+}
+
+/*todo: consider making this global*/
+</style>
