aaa9349611a4ae88dafc73fd33f9c7fd67e235db
Allen <azhang307@gatech.edu>
2019-07-24 17:19:41 -0400

    Arranged the port checkboxes in a grid to make it look a little nicer.

--- web/frontend-bs/src/components/test/AllenTest.vue
+++ web/frontend-bs/src/components/test/AllenTest.vue
@@ -1,3 +1,4 @@
+
 <template>
 
   <div>
@@ -18,22 +19,138 @@
           Your selection: {{form.action}}
         </td>
         <td>
-          <!-- Check All -->
-          <input type='checkbox' @click='checkAll()' v-model='isCheckAll'> Check All
+          <div>
+            <!-- Check All -->
+            <input type='checkbox' @click='checkAll()' v-model='isCheckAll'> Check All
 
-          <!-- Checkboxes list -->
-          <ul>
-            <li v-for='port in portList'>
-              <input type='checkbox' v-bind:value='port' v-model='ports' @change='updateCheckall()'>{{ port }}
-            </li>
-          </ul>
+            <!-- Checkboxes list -->
+            <table width="400">
+            <tr v-for='row in portList'>
+              <td v-for='port in row'>
+                <input type='checkbox' v-bind:value='port' v-model='ports' @change='updateCheckall()'>
+                <label :for="port"> {{port}}</label>
+              </td>
+            </tr>
+            </table>
 
-          <!-- Print -->
-          <input type='button' @click='printValues()' value='Select these Ports'>
 
+            <!-- Print -->
+            <input type='button' @click='printValues()' value='Select these Ports'>
+
+            <br>
+
+              Selected items : {{ form.selectedPorts }}
+
+
+
+          </div>
+        </td>
+        <td>
+          <b-modal id="bcmportwindow" scrollable>
+            <h1>
+              text
+              scroll
+            </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1><h1>
+            text
+            scroll
+          </h1>
+          </b-modal>
+          <button type="button"
+                  class="btn btn-dark btn-sm"
+                  v-b-modal="'bcmportwindow'"
+          >
+            <font-awesome-icon :icon="['fas', 'trash-alt']"
+                               size=sm
+                               color='rgb(255, 255, 255)'/>
+            Remove
+          </button>
+        </td>
+        <td>
+          <input
+            class="form-control"
+            v-model="form.vlanid"
+            id="VLAN ID"
+            placeholder="VLAN ID">
+          <div class="text-danger">
+            <ul>
+              <li v-if="formErrors.badCharacters">VLAN ID must be a number.
+              </li>
+              <li v-if="formErrors.nameIsEmpty">You must specify a VLAN ID.
+              </li>
+            </ul>
+          </div>
           <br>
-          Selected items : {{ form.selectedPorts }}
-
+          <button type="button"
+                  @click="testButton"
+                  :ok-disabled="!isFormValid"
+                  class="btn btn-dark btn-sm">
+            Do the thing
+          </button>
+          <br>
+          {{test}}
         </td>
 
 
@@ -55,17 +172,30 @@
                 checkedNames: [],
                 test: "Hello",
                 isCheckAll: false,
-                portList: ["ge1", "ge2", "ge3", "ge4", "ge5", "ge6", "ge7", "ge8", "ge9", "ge10",
-                    "ge11", "ge12", "ge13", "ge14", "ge15", "ge16", "ge17", "ge18", "ge19", "ge20",
-                    "ge21", "ge22", "ge23", "ge24", "ge25", "ge26", "ge27", "ge28", "ge29", "ge30",
-                    "ge31", "ge32", "ge33", "ge34", "ge35", "xe0", "xe1", "xe2", "xe3", "xe4",
-                    "xe5", "xe6", "xe7", "xe8", "xe9", "xe10", "xe11", "xe12"],
+                portList: [["ge1", "ge2", "ge3", "ge4"],
+                    ["ge5", "ge6", "ge7", "ge8"],
+                    ["ge9", "ge10", "ge11", "ge12"],
+                    ["ge13", "ge14", "ge15", "ge16"],
+                    ["ge17", "ge18", "ge19", "ge20"],
+                    ["ge21", "ge22", "ge23", "ge24"],
+                    ["ge25", "ge26", "ge27", "ge28"],
+                    ["ge29", "ge30", "ge31", "ge32"],
+                    ["ge33", "ge34", "ge35"],
+                    ["xe0", "xe1", "xe2", "xe3"],
+                    ["xe4", "xe5", "xe6", "xe7"],
+                    ["xe8", "xe9", "xe10", "xe11"], ["xe12"]],
                 ports: [],
                 form: {
                     selectedPorts: "",
-                    action: "Show VLANs"
+                    action: "Show VLANs",
+                    vlanid: ""
                 },
-                actionsAvailable: ["Show VLANs", "Create VLAN"]
+                formErrors: {
+                    badCharacters: false,
+                    fieldIsEmpty: true
+                },
+                actionsAvailable: ["Show VLANs", "Create VLAN", "Destroy VLAN", "Add ports to VLAN",
+                    "Remove ports from VLAN", "Show port-based VLANs", "Set port-based VLANs"]
             }
         },
         // mounted() {
@@ -75,19 +205,34 @@
         //         console.log(this.test)
         //     })
         // },
+        computed: {
+            vlanid() {
+                return this.form.vlanid
+            },
+            selection(){
+                return this.form.action
+            },
+            isFormValid() {
+                return Object.values(this.formErrors).every(error => !error)
+            }
+        },
         methods: {
             checkAll: function () {
 
                 this.isCheckAll = !this.isCheckAll;
                 this.ports = [];
                 if (this.isCheckAll) { // Check all
-                    for (var key in this.portList) {
-                        this.ports.push(this.portList[key]);
+                    for(var row = 0; row < this.portList.length; row++) {
+                        for (var key in this.portList[row]) {
+                            console.log(key);
+                            this.ports.push(this.portList[row][key]);
+                        }
                     }
                 }
             },
             updateCheckall: function () {
-                if (this.ports.length == this.portList.length) {
+                const length = 48;
+                if (this.ports.length == length) {
                     this.isCheckAll = true;
                 } else {
                     this.isCheckAll = false;
@@ -100,10 +245,36 @@
                 for (key = 0; key < this.ports.length - 1; key++) {
                     this.form.selectedPorts += this.ports[key] + ", ";
                 }
-                if (this.ports.length>0) {
+                if (this.ports.length > 0) {
                     this.form.selectedPorts += this.ports[key];
                 }
+            },
+            testButton: function () {
+                this.test = "Button has been pressed!";
+            },
+            validateName() {
+                console.log("called Validate")
+                this.formErrors.badCharacters = this.form.vlanid.length > 0 && !this.form.vlanid.match(/^[0-9]*$/);
+                this.formErrors.nameIsEmpty = this.form.vlanid.length === 0 && (this.form.action != "Show VLANs"
+                    && this.form.action != "Show port-based VLANs");
+            }
+        },
+        watch: {
+            vlanid: function () {
+                this.validateName()
+            },
+            selection: function() {
+                this.validateName()
             }
         }
     }
 </script>
+
+<style>
+  .dropdown-menu {
+    height: auto;
+    max-height: 200px;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+</style>
