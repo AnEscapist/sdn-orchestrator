bb7e637a69b4c73c6d216e668d3f2c810e987821
Roger Jin <rogerjin@mit.edu>
2019-07-26 01:12:13 -0400

    try again chmod

--- ucpe/grpc_data_collector/modify_functions.py
+++ ucpe/grpc_data_collector/modify_functions.py
@@ -43,7 +43,7 @@ def ovs_add_port(bridge, if_port, type):
                     f'type={type}'])
     ovs_socket = f'/usr/local/var/run/openvswitch/{if_port}'
     subprocess.run(['sudo', 'chmod', '777', ovs_socket])
-    print("ran chmod")
+    print("ran chmod on", ovs_socket)
     print(os.path.exists(ovs_socket))
     return True
 
