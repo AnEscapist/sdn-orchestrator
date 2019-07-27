85f489d4497a65105cdf16bbd2fe69b25fc6a0c3
jesse lin <jl459k@att.com>
2019-07-26 20:25:04 -0400

    sriov

--- ucpe/grpc_data_collector/get_functions.py
+++ ucpe/grpc_data_collector/get_functions.py
@@ -229,7 +229,7 @@ def sriov_numvfs(device):
 def lshw_get_businfo():
     proc = subprocess.Popen(['sudo', 'lshw', '-c', 'network', '-businfo'], stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
-    time.sleep(100)
+    time.sleep(2)
     return str(proc)
 
 
