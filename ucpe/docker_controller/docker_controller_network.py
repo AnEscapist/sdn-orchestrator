3d00d755b7ecfd4903f2cc730baecf33ba5a9122
AnEscapist <ustcjzq@gmail.com>
2019-07-28 23:05:42 -0400

    test create network function

--- ucpe/docker_controller/docker_controller_network.py
+++ ucpe/docker_controller/docker_controller_network.py
@@ -33,7 +33,7 @@ def inspect_network(network_id):
 
 
 
-def create_network(network_name, driver, scope, subnet, gateway):
+def create_network(network_name, driver, scope, subnet, gateway, enable_ipv6):
     func = create_network
     try:
         if subnet and gateway:
@@ -45,9 +45,9 @@ def create_network(network_name, driver, scope, subnet, gateway):
                 pool_configs=[ipam_pool]
             )
             network = dcli.networks.create(name=network_name, driver=driver, scope=scope,
-                                           ipam=ipam_config)
+                                           ipam=ipam_config, enable_ipv6=enable_ipv6)
         else:
-            network = dcli.networks.create(name=network_name, driver=driver, scope=scope)
+            network = dcli.networks.create(name=network_name, driver=driver, scope=scope, enable_ipv6=enable_ipv6)
     except docker.errors.APIError as ae:
         return api_error(ae, func)
     return create_network_message(network.id, func)
